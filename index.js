5b41f6b9a7d56558bd766be3d0108ebc74a3a0b2
Radu Oprea <radu@raduoprea.com>
2016-04-07 23:35:29 +0100

    Now handles single / multi team scenarios.

--- index.js
+++ index.js
@@ -1,93 +1,29 @@
 var Botkit = require('botkit')
 var Witbot = require('witbot')
-
-var slackToken = process.env.SLACK_TOKEN
+var token = process.env.SLACK_TOKEN
 var witbot = Witbot(process.env.WIT_TOKEN)
+var controller = Botkit.slackbot({ debug: false });
 
-if (!slackToken) {
-  console.error('SLACK_TOKEN is required!')
-  process.exit(1)
+
+if (token) {
+  console.log("Starting in single-team mode")
+  controller.spawn({
+    token: token
+  }).startRTM(function(err,bot,payload) {
+    if (err) {
+      throw new Error(err);
+    }
+  });
+} else {
+  console.log("Starting in Beep Boop multi-team mode")
+  require('beepboop-botkit').start(controller, { debug: true })
 }
-
-var controller = Botkit.slackbot({ debug: false })
-// var controller = Botkit.slackbot()
-
-require('beepboop-botkit').start(controller);
 
 controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
   var wit = witbot.process(message.text, bot, message)
-  
+
   wit.hears('greetings', 0.5, function (bot, message, outcome) {
     bot.reply(message, 'Hola back!')
   })
-  
+
 })
-
-
-
-// witbot.hears('greeti', 0.5, function (bot, message, outcome) {
-//     bot.reply(message, "Hello to you too!")
-// })
-
-
-
-// ==
-
-// var bot = controller.spawn({
-//   token: slackToken
-// })
-
-// bot.startRTM(function (err, bot, payload) {
-//   if (err) {
-//     throw new Error('Could not connect to Slack')
-//   }
-// })
-
-// controller.on('bot_channel_join', function (bot, message) {
-//   bot.reply(message, "I'm here!")
-// })
-
-// controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-//   bot.reply(message, 'Hello.')
-// })
-
-// controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-//   bot.reply(message, 'Hello.')
-//   bot.reply(message, 'It\'s nice to talk to you directly.')
-// })
-
-// controller.hears('.*', ['mention'], function (bot, message) {
-//   bot.reply(message, 'You really do care about me. :heart:')
-// })
-
-// controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-//   var help = 'I will respond to the following messages: \n' +
-//       '`bot hi` for a simple message.\n' +
-//       '`bot attachment` to see a Slack attachment message.\n' +
-//       '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-//       '`bot help` to see this again.'
-//   bot.reply(message, help)
-// })
-
-// controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-//   var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-//   var attachments = [{
-//     fallback: text,
-//     pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-//     title: 'Host, deploy and share your bot in seconds.',
-//     image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-//     title_link: 'https://beepboophq.com/',
-//     text: text,
-//     color: '#7CD197'
-//   }]
-
-//   bot.reply(message, {
-//     attachments: attachments
-//   }, function (err, resp) {
-//     console.log(err, resp)
-//   })
-// })
-
-// controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-//   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
-// })
