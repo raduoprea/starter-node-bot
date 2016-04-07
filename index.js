36ec9b7872a554547bc9b8ed7ef82109d1c9ed47
Radu Oprea <radu@raduoprea.com>
2016-04-07 21:43:17 +0100

    Added witbot and the greetings wit.ai intent.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var Botkit = require('botkit')
+var Witbot = require("witbot")
 
 // Expect a SLACK_TOKEN environment variable
 var slackToken = process.env.SLACK_TOKEN
+var witToken = process.env.WIT_TOKEN
+
 if (!slackToken) {
   console.error('SLACK_TOKEN is required!')
   process.exit(1)
@@ -18,51 +21,66 @@ bot.startRTM(function (err, bot, payload) {
   }
 })
 
-controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
-})
-
-controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-})
-
-controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-  bot.reply(message, 'It\'s nice to talk to you directly.')
-})
-
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
-})
-
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
-})
-
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
+var witbot = Witbot(witToken)
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+  witbot.process(message.text, bot, message)
 })
+
+witbot.hears('greetings', 0.5, function (bot, message, outcome) {
+    bot.reply(message, "Hello to you too!")
+})
+
+
+
+
+// ==
+
+// controller.on('bot_channel_join', function (bot, message) {
+//   bot.reply(message, "I'm here!")
+// })
+
+// controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
+//   bot.reply(message, 'Hello.')
+// })
+
+// controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
+//   bot.reply(message, 'Hello.')
+//   bot.reply(message, 'It\'s nice to talk to you directly.')
+// })
+
+// controller.hears('.*', ['mention'], function (bot, message) {
+//   bot.reply(message, 'You really do care about me. :heart:')
+// })
+
+// controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
+//   var help = 'I will respond to the following messages: \n' +
+//       '`bot hi` for a simple message.\n' +
+//       '`bot attachment` to see a Slack attachment message.\n' +
+//       '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+//       '`bot help` to see this again.'
+//   bot.reply(message, help)
+// })
+
+// controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
+//   var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+//   var attachments = [{
+//     fallback: text,
+//     pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
+//     title: 'Host, deploy and share your bot in seconds.',
+//     image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+//     title_link: 'https://beepboophq.com/',
+//     text: text,
+//     color: '#7CD197'
+//   }]
+
+//   bot.reply(message, {
+//     attachments: attachments
+//   }, function (err, resp) {
+//     console.log(err, resp)
+//   })
+// })
+
+// controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
+//   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+// })
