ffb965bed2684eafd4ecf05d4dc544291bd59625
Radu Oprea <radu@raduoprea.com>
2016-04-08 12:28:34 +0100

    Gets the tasks marked for today and outputs them to the console.

--- tasks.js
+++ tasks.js
@@ -14,19 +14,46 @@ function NextTask() {
 
   self.nextTaskForUser = function(user, fn) {
     var client = asana.Client.create().useAccessToken(personalAccessToken);
-    var url = 'https://app.asana.com/api/1.0/tasks?workspace='+ workspaceId +
-              '&assignee=' + user;
     
-    request(url, function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body)
-        fn(null, body);
-      }
-    })
+    client.users.me()
+      .then(function(user) {
+        var userId = user.id;
+        // The user's "default" workspace is the first one in the list, though
+        // any user can have multiple workspaces so you can't always assume this
+        // is the one you want to work with.
+        var workspaceId = user.workspaces[0].id;
+        return client.tasks.findAll({
+          assignee: userId,
+          workspace: workspaceId,
+          completed_since: 'now',
+          opt_fields: 'id,name,assignee_status,completed'
+        });
+      })
+      .then(function(response) {
+        // There may be more pages of data, we could stream or return a promise
+        // to request those here - for now, let's just return the first page
+        // of items.
+        return response.data;
+      })
+      .filter(function(task) {
+        return task.assignee_status === 'today';
+      })
+      .then(function(list) {
+        console.log(util.inspect(list, {
+          colors: true,
+          depth: null
+        }));
+      });
     
-    // client.users.me().then(function(me) {
-    //   fn(null, me);
-    // });
+    // var url = 'https://app.asana.com/api/1.0/tasks?workspace='+ workspaceId +
+    //           '&assignee=' + user;
+    
+    // request(url, function(error, response, body) {
+    //   if (!error && response.statusCode == 200) {
+    //     console.log(body)
+    //     fn(null, body);
+    //   }
+    // })
   }
 
 }
