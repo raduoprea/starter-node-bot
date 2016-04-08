0118e483696814302e54342375aad88c076d49f2
Radu Oprea <radu@raduoprea.com>
2016-04-08 12:11:15 +0100

    Testing get my tasks.

--- index.js
+++ index.js
@@ -6,38 +6,40 @@ var controller = Botkit.slackbot({ debug: false });
 var tasks = require('./tasks')();
 
 if (token) {
-    console.log("Starting in single-team mode")
-    controller.spawn({
-        token: token
-    }).startRTM(function(err, bot, payload) {
-        if (err) {
-            throw new Error(err);
-        }
-    });
+  console.log("Starting in single-team mode")
+  controller.spawn({
+    token: token
+  }).startRTM(function(err, bot, payload) {
+    if (err) {
+      throw new Error(err);
+    }
+  });
 } else {
-    console.log("Starting in Beep Boop multi-team mode")
-    require('beepboop-botkit').start(controller, { debug: true })
+  console.log("Starting in Beep Boop multi-team mode")
+  require('beepboop-botkit').start(controller, { debug: true })
 }
 
 controller.hears('.*', 'direct_message,direct_mention', function(bot, message) {
-    var wit = witbot.process(message.text, bot, message)
+  var wit = witbot.process(message.text, bot, message)
 
-    wit.hears('greetings', 0.5, function(bot, message, outcome) {
-        bot.reply(message, 'Hola back!')
-    })
+  wit.hears('greetings', 0.5, function(bot, message, outcome) {
+    bot.reply(message, 'Hello to you too!')
+  })
 
-    wit.hears('next_task', 0.5, function(bot, message, outcome) {
+  wit.hears('next_task', 0.5, function(bot, message, outcome) {
 
-        tasks.nextTask (function (error, msg) {
-            if (error) {
-                bot.reply(message, 'There was a problem: ' + error);
-            }
-            bot.reply(message, msg);
-        });
-    })
+    tasks.nextTaskForUser( 'me', function(error, msg) {
+      if (error) {
+        bot.reply(message, 'There was a problem: ' + error);
+      } else {
+        bot.reply(message, msg);
+      }
 
-    wit.hears('tasks_for_today', 0.5, function(bot, message, outcome) {
-        bot.reply(message, 'Here is a list of your tasks for today.');
-    })
+    });
+  })
+
+  wit.hears('tasks_for_today', 0.5, function(bot, message, outcome) {
+    bot.reply(message, 'Here is a list of your tasks for today.');
+  })
 
 })
--- tasks.js
+++ tasks.js
@@ -1,21 +1,32 @@
+var request = require('request');
 var asana = require('asana');
 var clientId = process.env.ASANA_CLIENT_ID;
 var clientSecret = process.env.ASANA_CLIENT_SECRET;
 var personalAccessToken = process.env.ASANA_TOKEN;
+var workspaceId = process.env.ASANA_WORKSPACE_ID
 
-module.exports = function () {
+module.exports = function() {
   return new NextTask();
 }
 
-function NextTask () {
-    var self = this;
+function NextTask() {
+  var self = this;
+
+  self.nextTaskForUser = function(user, fn) {
+    var client = asana.Client.create().useAccessToken(personalAccessToken);
+    var url = 'https://app.asana.com/api/1.0/tasks?workspace='+ workspaceId +
+              '&assignee=' + user;
     
-    self.nextTask = function(fn) {
-        var client = asana.Client.create().useAccessToken(personalAccessToken);
-        client.users.me().then(function(me) {
-            console.log(me);
-            fn(null, me);
-        });
-    }
-      
+    request(url, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body)
+        fn(null, body);
+      }
+    })
+    
+    // client.users.me().then(function(me) {
+    //   fn(null, me);
+    // });
+  }
+
 }
